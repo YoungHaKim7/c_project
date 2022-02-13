36b38bbdc9057bf9a25c2c7b93040c74b8a23423
YoungHa Kim <ytok1108@aol.com>
2022-02-14 01:29:47 +0900

    add

--- hello.c
+++ hello.c
@@ -3,11 +3,14 @@
 int main()
 {
     int a, b;
-    printf("Enter number a ");
+    printf("Enter number a\n ");
     scanf("%d", &a);
 
-    printf("Enter number b");
+    printf("Enter number b\n");
     scanf("%d", &b);
+
+    printf("The sum is %d\n", a+b);
 
     return 0;
 }
+
