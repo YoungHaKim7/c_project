22d1d6c600e71c270ee2638a95a4e91c35e9318b
YoungHa Kim <ytok1108@aol.com>
2022-02-14 02:06:15 +0900

    %d print& sizeof

--- hello.c
+++ hello.c
@@ -2,15 +2,21 @@
 
 int main()
 {
-    int a, b;
-    printf("Enter number a\n ");
-    scanf("%d", &a);
+    int a = 7; // 2 to 4bytes
+    unsigned short integer = 8;// 2bytes
+    long integer1 = 8;// 4 bytes
+    short integer2 = 8;// 2 bytes
 
-    printf("Enter number b\n");
-    scanf("%d", &b);
+    
+    float b = 8.0; // 4byte - 6 decimal precision:
+    double myfloat1 = 7.45; // 8 bytes - 15 decimal places precision
+    long double myfloat2 = 7.43453455; //10 bytes - 19 decimal places precision
+    char c = 't'; // 1byte
 
-    printf("The sum is %d\n", a+b);
+    printf("The size taken by int is %d", sizeof(int));
 
+    printf("hello %c", c);
     return 0;
+        
 }
 
