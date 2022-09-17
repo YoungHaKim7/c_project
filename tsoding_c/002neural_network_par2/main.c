f75d2454b58c4f1d56f00fff0a449b104cf8404e
YoungHa Kim <ytok1108@aol.com>
2022-09-17 22:19:39 +0900

    First Ancient Neural Network in C result: output = 0.000000

---
+++ tsoding_c/002neural_network_par2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define WIDTH 20
+#define HEIGHT 20
+
+typedef float Layer[HEIGHT][WIDTH];
+
+float feed_forward(Layer inputs, Layer weights)
+{
+  float output = 0.0f;
+  
+  for (int y = 0; y < HEIGHT; ++y) {
+    for (int x = 0; x < WIDTH; ++x) {
+      output += inputs[y][x] * weights[y][x];
+    }
+  }
+  
+  return output;
+}
+
+static Layer inputs;
+static Layer weights;
+
+int main(void) {
+  float output = feed_forward(inputs, weights);
+  printf("output = %f\n", output);
+  
+  return 0;
+}
