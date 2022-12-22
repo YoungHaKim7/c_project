ffce538289c20abf8ab3063f9dd0561d0ab63a24
YoungHa Kim <ytok1108@aol.com>
2022-12-22 21:39:53 +0900

    C lang linked list

--- algorithm/data_structure/single_linked_list1/README.md
+++ algorithm/data_structure/single_linked_list1/README.md
@@ -1,3 +1,11 @@
+# c언어로 만든 싱글 링크드 리스트 linked list ( 추가, 삽입 , 삭제) 백지부터 설명 시작
+
+https://youtu.be/3ZdafcIvREw
+
+<br>
+
+<hr>
+
 # Creating the Node of a Single Linked List
 
 https://youtu.be/DneLxrPmmsw
--- algorithm/data_structure/single_linked_list1/main.c
+++ algorithm/data_structure/single_linked_list1/main.c
@@ -1,23 +1,50 @@
+// malloc.h source
+// https://opensource.apple.com/source/Libc/Libc-825.26/include/malloc/malloc.h.auto.html
 #include <stdio.h>
 #include <stdlib.h>
+#include "malloc.h"
 
-struct node {
+typedef struct Node {
   int data;
-  struct node *link;
-};
+  struct Node *next;
+}Node;
 
-int main() {
-  struct node *head = NULL;
-  head = (struct node *)malloc(sizeof(struct node));
-
-  head->data = 45;
-  head->link = NULL;
-
-  printf("%d\n", head->data);
-  return 0;
+void add(Node **head, Node **tail, int data){
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data; // 1 2
+  newNode->next = newNode; //NULL;
+  if(!*head || !tail){
+    *head = *tail = newNode;
+  }else{
+    newNode->next = *head;
+    *head = newNode;
+    (*tail)->next = *head;
+  }
 }
 
-// result:
-// $ ./main
-// 45  
+void del(){}
+
+void insert(){}
+
+// *head reference address (*tail)->next;
+void printList(Node *head, Node *tail){
+  while(head->next != tail ->next){ // Until head's next address is head himself
+    printf("%d", head->data); // 5 4 3 2 1 The initial address of the head
+    head = head->next;
+  }
+  printf("%d", head->data);
+}  
+
+int main() {
+  Node *head = NULL;
+  Node *tail = NULL;
+  int i;
+  for(i=0;i<5;i++){
+    add(&head, &tail , i+1);
+  }
+  printList(head, tail);
+  return 0;
+
+}
+
 
