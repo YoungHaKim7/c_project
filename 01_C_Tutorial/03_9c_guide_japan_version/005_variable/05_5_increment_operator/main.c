8b62c206dd39a596812c6f5ceeca05b60888180d
YoungHa Kim <ytok1108@aol.com>
2023-09-11 00:21:28 +0900

    increment decrement opterator

---
+++ 01_C_Tutorial/03_9c_guide_japan_version/005_variable/05_5_increment_operator/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int value;
+  value = 10;
+  printf("%d\n", value);
+  value++;
+  printf("%d\n", value);
+  value--;
+  printf("%d\n", value);
+  return 0;
+}
