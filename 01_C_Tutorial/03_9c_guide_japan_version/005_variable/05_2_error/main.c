0f08adb97fff51d7704047b096e4bad96486a8bb
YoungHa Kim <ytok1108@aol.com>
2023-09-11 00:15:13 +0900

    printf

---
+++ 01_C_Tutorial/03_9c_guide_japan_version/005_variable/05_2_error/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello\n");
+  int value;
+  return 0;
+}
---
+++ 01_C_Tutorial/03_9c_guide_japan_version/005_variable/05_3_printf/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int left;
+    int right;
+    left = 10;
+    right = 3;
+    printf("%d\n", left + right);
+    printf("%d\n", left - right);
+    printf("%d\n", left * right);
+    printf("%d\n", left / right);
+    printf("%d\n", left % right);
+    return 0;
+}
